f172a9e4011783e99899555809270be1e08629ea
Cinque <ba.peereboom@gmail.com>
2026-06-10 15:44:05 +0200

    Add simple book fetching example

---
+++ src/main/kotlin/Book.kt
@@ -0,0 +1,8 @@
+package nl.rhaydus
+
+import io.ktor.resources.Resource
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Resource("/books")
+data class Book(val title: String = "", val author: String = "")
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -3,30 +3,16 @@ package nl.rhaydus
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.resources.*
-import io.ktor.server.resources.*
-import io.ktor.server.resources.Resources
-import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello, World!")
-        }
+        get("/books") {
+            val books = listOf(
+                Book(title = "Salem's lot", author = "Stephen King"),
+                Book(title = "Salem's lot", author = "Stephen King"),
+            )
 
-        get("/softcover") {
-            call.respond("This is softcover's endpoint!")
-        }
-
-        get("/x") {
-            call.respond("test!")
-        }
-        get<Articles> { article ->
-            // Get all articles ...
-            call.respond("List of articles sorted starting from ${article.sort}")
-        }
-        get("/json/kotlinx-serialization") {
-            call.respond(mapOf("hello" to "world"))
+            call.respond(books)
         }
     }
 }
